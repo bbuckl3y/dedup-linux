b187b913ae21f8dcf977977fc1bec80aac097e2f
Bob Buckley <bob@buckley.id.au>
2023-11-25 22:25:35 +1100

    Create dedup.py

---
+++ dedup.py
@@ -0,0 +1,18 @@
+"""
+Find duplicate graphics files.
+Graphics files have certain finename extensions, e.g. .jpg or .png
+Later, they may be linked so there is only one copy.
+"""
+import os
+import argparse
+
+def main()
+    """
+    DeDup main routine.
+    """
+    os.chdir("/media/samba/s4") # use argparse later ...
+    
+    return
+
+if __name__ == "__main__"
+    main()
